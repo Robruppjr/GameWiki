85dc212e95c41eeb8b89bf876158817c1c500ecb
Rob Rupp <robruppjr@gmail.com>
2022-06-22 17:53:31 -0400

    CreateGame Endpoint

--- GameWikiAPI.WebAPI/Program.cs
+++ GameWikiAPI.WebAPI/Program.cs
@@ -11,6 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IGameService, GameService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => 
